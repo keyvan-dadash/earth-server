06b06920116e568ff255337e25c65ee0e258e19a
S.O.D <ddssee5511@gmail.com>
2020-12-11 00:14:32 -0500

    docker ignore added, cassandra db added to docker-compose

--- cmd/earth/main.go
+++ cmd/earth/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/gocql/gocql"
 	"github.com/sod-lol/earth-server/internal/app/authentication"
 	"github.com/sod-lol/earth-server/pkg/redis"
 )
 
+func checkDB() {
+	cluster := gocql.NewCluster("cassandra")
+	cluster.Keyspace = "example"
+	cluster.Consistency = gocql.Quorum
+	session, _ := cluster.CreateSession()
+	defer session.Close()
+
+	// insert a tweet
+	if err := session.Query(`INSERT INTO tweet (timeline, id, text) VALUES (?, ?, ?)`,
+		"me", gocql.TimeUUID(), "hello world").Exec(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+
+	checkDB()
 
 	redisAuth := redis.CreateRedisClient("redis-auth:6379", "", 0)
 
